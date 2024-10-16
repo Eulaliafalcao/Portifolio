c77ab031eb01534858de233411aa8de7146720f8
Eulaliafalcao <meulaliadeoliveirafalcao@gmail.com>
2024-10-16 17:32:12 -0300

    Add files via upload

---
+++ historias.js
@@ -0,0 +1,25 @@
+criaCartao(
+    'No 1° do fundamental',
+    'Dia de sassão de fotos',
+    'No 1° ano do fundamental, tinha os dias de sessões de fotos dos alunos, onde eles vestiam roupas diferentes do uniforme escolar para nossos pais comprarem e terem uma lembrança.'
+
+)
+
+criaCartao(
+    'Formatura 5° ano',
+    'Formatura do fundamental',
+    'No 5° ano, minha antiga escola municipal decidiu fazer uma formatura para os alunos terem uma despedida marcante.'
+)
+
+criaCartao(
+    'Passeio',
+    'Passeio de Teatro',
+    'No 9° ano, eu havia acabado de entrar noo colégio em setembro, em outbro tivemos o passeio para o teatro Guaira, onde eu e minhas amigas assistimos uma peça. '
+
+)
+
+criaCartao(
+    'Nova foto do grupo',
+    'Nossa nova foto de grupo',
+    'Nós estavamos tendo aula de fisica, onde estavamos fazendo as montagens dos circuitos, até que eu tive a ideia de tirar uma foto com minhas amigas, já que estavamos esperando o profeesor nos entregar os matériais necessários para fazermos as montagens e eu estava sem nenhuma foto com minhas amigas desse ano, então peguei o celular e tirei duas fotos. Uma semana depois, eu usei uma delas como nova foto do grupo. '
+)
